78d90bb03d8d11f2e1576dac94aa8fa69bf347e1
NFilin <nickadm@yandex.ru>
2022-07-01 09:06:42 +0300

    fix: updateEpicById добавил в него проверку determineAndSetCorrectEpicStatus();

--- src/Manager.java
+++ src/Manager.java
@@ -149,6 +149,7 @@ public class Manager {
         } else {
             System.out.println("Такого эпика нет!");
         }
+        determineAndSetCorrectEpicStatus();
     }
 
     public void updateSubtaskById(Integer idSubtask, Subtask newSubtask) {
