b8c22c36ba9016366daaf5148be434e4dca2bc5f
NFilin <nickadm@yandex.ru>
2022-10-03 15:12:52 +0300

    style: поправил стиль кода

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,12 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         TaskManager taskManager = Managers.getDefault();
-
         Task task1 = new Task(
                 "Test name",
                 "Test description",
                 Status.NEW,
                 "05.05.2022 14:03",
                 90);
-
         taskManager.writeNewTask(task1);
         Task task2 = new Task(
                 "Test name",
@@ -24,7 +22,6 @@ public class Main {
                 Status.NEW,
                 "05.09.2022 14:03",
                 90);
-
         taskManager.writeNewTask(task2);
         taskManager.getTaskById(1);
         taskManager.getTaskById(2);
--- src/management/FileBackedTasksManagerTest.java
+++ src/management/FileBackedTasksManagerTest.java
@@ -7,10 +7,44 @@ import java.io.File;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FileBackedTasksManagerTest {
-
+    TaskManager taskManager = Managers.loadFromFile(new File("src/resources/loadfortest.csv"));
     @Test
     void loadFileCheckHistory() {
-        TaskManager taskManager = Managers.loadFromFile(new File("src/resources/history673145.csv"));
-        assertEquals(taskManager.getCurrentHistoryOnlyId(), "6,7,3,1,4,5", "Ошибка в загрузке истории");
+        assertEquals(taskManager.getCurrentHistoryOnlyId(), "6,7,4,3,5,1", "Ошибка в загрузке истории");
+    }
+
+    @Test
+    void checkTackStartTime() {
+        assertEquals(taskManager.getTaskById(1).getStartTime(), "01.10.2020 14:03", "ошибка Task поле startTime");
+    }
+
+    @Test
+    void checkTackEndTime() {
+        assertEquals(taskManager.getTaskById(1).getDuration(), 30, "ошибка Task поле duration");
+    }
+
+    @Test
+    void checkEpicStartTime() {
+        assertEquals(taskManager.getEpicById(3).getStartTime(), "01.10.2020 14:03", "ошибка Task поле startTime");
+    }
+
+    @Test
+    void checkEpicEndTime() {
+        assertEquals(taskManager.getEpicById(3).getDuration(), 30, "ошибка Task поле duration");
+    }
+
+    @Test
+    void checkSubtaskStartTime() {
+        assertEquals(taskManager.getSubtaskById(5).getStartTime(), "01.10.2020 14:03", "ошибка Subtask поле startTime");
+    }
+
+    @Test
+    void checkSubtaskEndTime() {
+        assertEquals(taskManager.getSubtaskById(5).getDuration(), 30, "ошибка Subtask поле duration");
+    }
+
+    @Test
+    void checkSubtaskLinkEpic() {
+        assertEquals(taskManager.getSubtaskById(5).getLinkEpic(), 3, "ошибка Subtask связь с Epic");
     }
 }
